df2bf7fc0f70ecbb8dc28cfa52cc3fd039ea1e39
raedremi <sham1987@syria@gmail.com>
2026-06-22 02:10:37 +0300

    add real content

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,42 @@
 <template>
   <footer class="footer">
 
-    <h2>المهندس التقني</h2>
+    <h2>المهندس التقني للمعلوماتية</h2>
 
     <p>
-      شروحات وبرامج ودعم فني وحلول تقنية.
+      شروحات وبرامج ودعم فني وحلول تقنية للمستخدم العربي.
     </p>
 
     <div class="links">
       <span>برامج كمبيوتر</span>
       <span>برامج محاسبية</span>
       <span>الدعم الفني</span>
+    </div>
+
+    <div class="contact-links">
+
+      
+      <a
+        href="https://wa.me/966596276143"
+        target="_blank"
+      >
+       واتساب فرع السعودية :00966596276143
+      </a>
+      <a
+        href="https://wa.me/963982188637"
+        target="_blank"
+      >
+        واتساب فرع سوريا:00963982188637 
+      </a>
+
+
+       <a
+        href="https://wa.me/971507684824"
+        target="_blank"
+      >
+       واتساب فرع الامارات :00971507684824
+      </a>
+
     </div>
 
     <p class="copy">
@@ -47,5 +73,22 @@
 
 .copy{
   opacity:.8;
+}
+
+.contact-links{
+  display:flex;
+  flex-direction:column;
+  gap:10px;
+
+  margin-bottom:25px;
+}
+
+.contact-links a{
+  color:white;
+  text-decoration:none;
+}
+
+.contact-links a:hover{
+  text-decoration:underline;
 }
 </style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -4,26 +4,90 @@
 
     <h1>تواصل معنا</h1>
 
-    <p>
-      يمكنك التواصل معنا عبر:
+    <p class="intro">
+      يسعدنا استقبال استفساراتكم المتعلقة ببرامج 
+       المحاسبية، والدعم الفني.
     </p>
 
-    <p>
-      البريد الإلكتروني:
-    </p>
+    <div class="contact-card">
 
-    <p>
-      info@sham-tech.com
-    </p>
+      <h3> واتساب السعودية</h3>
+ <a
+        href="https://wa.me/966596276143"
+        target="_blank"
+      >
+        مراسلة الدعم الفني
+      </a>
+
+      <!-- <a href="mailto:info@sham-tech.com">
+        info@sham-tech.com
+      </a> -->
+
+    </div>
+
+    <div class="contact-card">
+
+      <h3>واتساب الإمارات </h3>
+
+      <a
+        href="https://wa.me/971507684824"
+        target="_blank"
+      >
+        مراسلة الدعم الفني
+      </a>
+
+    </div>
+
+    <div class="contact-card">
+
+      <h3>واتساب  سوريا</h3>
+
+      <a
+        href="https://wa.me/963982188637"
+        target="_blank"
+      >
+        مراسلة الدعم الفني
+      </a>
+
+    </div>
 
   </div>
 
 </template>
 
 <style scoped>
+
 .container{
-  max-width:1000px;
+  max-width:800px;
   margin:auto;
-  padding:40px;
+  padding:60px 20px;
+  text-align:center;
 }
+
+h1{
+  margin-bottom:20px;
+}
+
+.intro{
+  margin-bottom:40px;
+  color:#555;
+}
+
+.contact-card{
+  background:#f8f9fa;
+  padding:25px;
+  margin-bottom:20px;
+  border-radius:12px;
+}
+
+.contact-card a{
+  color:#0f172a;
+  text-decoration:none;
+  font-weight:bold;
+}
+
+.contact-card a:hover{
+  text-decoration:underline;
+}
+
 </style>
